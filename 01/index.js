b9aa96f541820d25cde2e87ec2a2b2077508ffe8
andre347 <aedevries19@gmail.com>
2019-12-02 16:40:48 +0000

    .. now I got the right answer

--- 01/index.js
+++ 01/index.js
@@ -4,17 +4,17 @@
 const fs = require("fs");
 const inputFile = "input.txt";
 
+// part one
 fs.readFile(inputFile, "utf8", (err, data) => {
   // split the text file on the newLine
   const parsedData = data.split("\n");
   const fuelData = parsedData.map(d => fuelCalculator(d));
   // sum up the values returned from the fuel Calc function
   const sum = fuelData.reduce((a, b) => a + b, 0);
-  // console.log the sum
   console.log(sum);
   return sum;
 });
 
 function fuelCalculator(d) {
-  return +Math.floor(d / 3) - 2;
+  return d > 0 ? +Math.floor(+d / 3) - 2 : null;
 }
