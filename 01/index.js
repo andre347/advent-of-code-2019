daac503e6ecfeb13e72f3b7b62b24bd24cd9c782
andre347 <aedevries19@gmail.com>
2019-12-02 22:28:29 +0000

    finished section 2 day 1

--- 01/index.js
+++ 01/index.js
@@ -8,13 +8,25 @@ const inputFile = "input.txt";
 fs.readFile(inputFile, "utf8", (err, data) => {
   // split the text file on the newLine
   const parsedData = data.split("\n");
-  const fuelData = parsedData.map(d => fuelCalculator(d));
+  const fuelData = parsedData.map(d => loopValue(d));
   // sum up the values returned from the fuel Calc function
   const sum = fuelData.reduce((a, b) => a + b, 0);
+  //   console.log(loopValue(1969));
   console.log(sum);
-  return sum;
+  //   return sum;
 });
 
 function fuelCalculator(d) {
   return d > 0 ? +Math.floor(+d / 3) - 2 : null;
 }
+
+const loopValue = function calcValue(d, storeValues) {
+  let arr = storeValues || [];
+  if (d > 6) {
+    const newValue = d > 0 ? +Math.floor(+d / 3) - 2 : null;
+    arr.push(newValue);
+    return loopValue(newValue, arr);
+  } else {
+    return arr.reduce((a, b) => a + b, 0);
+  }
+};
